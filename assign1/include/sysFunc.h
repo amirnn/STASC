3228d1ea5c8fec65b8a2b6c5d7f12776777c1b62
Amir Nourinia <amirnourinia@Amirs-MacBook-Pro.local>
2020-04-28 02:40:17 +0200

    tested with some analytical solutions.

--- assign1/include/sysFunc.h
+++ assign1/include/sysFunc.h
@@ -7,13 +7,13 @@
 namespace SysFunc{
 using namespace STASC;
 using namespace std::complex_literals;
-constexpr std::complex<double> lambda1(0,2);
-constexpr std::complex<double> lambda2(0,-0.5);
-State initialStateOne {.5,-1/3};
+constexpr std::complex<double> lambda1(1,0);
+constexpr std::complex<double> lambda2(-1,0);
+State initialStateOne {1,0};
 State sysFuncOne(const State &state, double time)
 {
     State gradient(state.size());
-    gradient[0] = -lambda1 * state[1];
+    gradient[0] = lambda1 * state[1];
     gradient[1] = lambda2 * state[0];
     return gradient;
 }
